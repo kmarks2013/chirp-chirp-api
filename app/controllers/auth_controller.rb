769bb023f378cd06ef69b7dcea752c5e215b0abc
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 18:29:25 -0500

    adds authentication method to render user or errors

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,6 +1,13 @@
 class AuthController < ApplicationController
 
     def login
+        user = User.find_by(username: params[:username])
+        is_authenticated = user.authenticate(params[:password])
+        if is_authenticated
+            render json: user
+        else
+            render json: {error: "Wrong username/password. Please try Again"}
+        end
     end
 
 end

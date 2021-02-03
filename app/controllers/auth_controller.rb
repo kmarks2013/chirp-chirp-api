58aad4699ec1f0ebd64f9098e9ddb91aef7aaf2f
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 21:53:34 -0500

    adds comment to add log in with email as well as username

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,6 +2,7 @@ class AuthController < ApplicationController
 
     def login
         user = User.find_by(username: params[:username])
+        # add a way to log in with email as well
         is_authenticated = user.authenticate(params[:password])
         if is_authenticated
             render json: user

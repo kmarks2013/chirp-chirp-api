5667548f21c97984132b2024cd43d0ddbd17de8b
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 21:49:45 -0500

    adds validation for presence of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     # email validations
     validates_presence_of :email
 
+    # username validations
+    validates_presence_of :username
 
     # password validations
     has_secure_password

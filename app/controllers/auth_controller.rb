8898f3bdc58e23d0890130d9cc819382b1d6206b
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 22:19:38 -0500

    adds JWT logic to login controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,7 +5,9 @@ class AuthController < ApplicationController
         # add a way to log in with email as well
         is_authenticated = user.authenticate(params[:password])
         if is_authenticated
-            render json: user
+            payload = {user_id: user.id}
+            token = JWT.encode(payload, nil, 'none')
+            render json: {token: token}
         else
             render json: {error: "Wrong username/password. Please try Again"}
         end

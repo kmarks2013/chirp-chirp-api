240d2753031a7d5f0bc00ca534334cd8ce831607
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 21:57:52 -0500

    adds validation to to length of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
     # username validations
     validates_presence_of :username
     validates_uniqueness_of :username, :case_sensitive => false
+    validates :username, :length => { :in => 6..30}
 
     # password validations
     has_secure_password

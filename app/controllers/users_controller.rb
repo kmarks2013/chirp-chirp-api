beaac26995897a66872c6f58a5f3041a76cdf674
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 21:42:57 -0500

    removes the params.require so that password is permitted by params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:email, :password, :username)
+        params.permit(:email, :password, :username)
     end
 
 end

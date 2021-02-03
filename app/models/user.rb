a9aa4b0a428970d52a783acb4cb9b7264d064152
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 21:54:49 -0500

    adds for uniqueness of email and no case sensitivity

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     # email validations
     validates_presence_of :email
+    validates_uniqueness_of :email, :case_sensitive => false
 
     # username validations
     validates_presence_of :username

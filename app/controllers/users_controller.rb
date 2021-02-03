53c36497e22c2081970ea7653418ba066613cf18
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 20:56:39 -0500

    adds user create method, and if user is valid renders the user json or errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,13 @@ class UsersController < ApplicationController
     end
 
     def create
-        user = User.create()
+        user = User.create(user_params)
+        byebug
+        if user.valid?
+            render json: user
+        else
+            render json: {errors: user.errors.full_messages}
+        end
     end
 
     private

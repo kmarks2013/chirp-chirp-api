f95f262b28235512d072abfe351e7936ae76886c
kmarks2013 <kmarks2013@gmail.com>
2021-02-02 22:41:00 -0500

    refactors to call on helper methods from app controller for jwt logic

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,9 +5,7 @@ class AuthController < ApplicationController
         # add a way to log in with email as well
         is_authenticated = user.authenticate(params[:password])
         if is_authenticated
-            payload = {user_id: user.id}
-            token = JWT.encode(payload, 'examplesecretkey', 'HS256')
-            render json: {token: token}
+            render json: {token: encode_token(user_payload(user))}
         else
             render json: {error: "Wrong username/password. Please try Again"}
         end
